bcaa30a2e551917ac7710d1c729f522b4da93e64
Sofie Bruun Knudsen <sofiebruunknudsen@gmail.com>
2020-11-07 10:41:51 +0100

    Completed the weekly assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates a list, the second function returns the inverse of the matrix
 
-## Write a short comment describing this function
+## The first function creates an R object that stores a list with several outputs of the matrix X.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invers <- NULL
+  set <- function(y){
+    x <<- y
+    invers <<- NULL
+  }
+get <- function() x
+setinverse <- function(inverse) invers <<- inverse
+getinverse <- function() invers
+list(set = set, get = get,
+     setinverse = setinverse,
+     getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function inverse the matrix x from makeCacheMatrix(). makeCacheMatrix() is incomplete without cacheSolve().
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  invers <- x$getinverse()
+  if(!is.null(invers)) {
+    message("getting cached data")
+    return(invers)
+  }
+  mat <- x$get()
+  invers <- solve(mat, ...)
+  x$setinverse(invers)
+  invers
+}        ## Return a matrix that is the inverse of 'x'
+
+
